6b2c967081581da97d1645546d3f2c36b4b5f9b0
e <13077661396@163.com>
2023-06-01 23:42:16 +0800

    11

--- od/B/fenshu200/OD02.java
+++ od/B/fenshu200/OD02.java
@@ -1,10 +1,94 @@
 package com.od.B.fenshu200;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 
+/*
+* 找最小数
+2615371
+4
+输出131  2615371删掉4个数，使得剩下的数值是最小的
+1110021
+4
+001
+循环比较开头两位数
+第二位比第一位大，就删第二位，否则删第一位，当剩下len+1的时候，删掉最大数值的。
+特殊情况考虑两位值相等的情况
+* */
 public class OD02 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        String s = sc.nextLine();
+        int m = sc.nextInt();
+
+        ArrayList<Integer> list = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            list.add(s.charAt(i) - '0');
+            set.add(s.charAt(i) - '0');
+        }
+
+        int len = s.length() - m;
+
+        //针对特殊情况 同一个值的字符串 如：111111
+        if (set.size() == 1) {
+            for (int i = 0; i < len; i++) {
+                System.out.print(list.get(0));
+            }
+            return;
+        }
+
+
+        while (list.size() > len + 1) {
+            Integer a = list.get(0);
+            Integer b = list.get(1);
+
+            if (a != b) {
+                if (b > a) {
+                    list.remove(1);
+                }
+
+                if (a > b) {
+                    list.remove(0);
+                }
+            } else { //针对开头值都是相同值
+
+                int index = 2;
+                for (int i = 2; i < list.size(); i++) {
+                    Integer c = list.get(i);
+                    if (c != b) {
+                        index = i;
+                        break;
+                    }
+                }
+                if (list.get(index - 1) > list.get(index)) {
+                    list.remove(index - 1);
+                } else {
+                    list.remove(index);
+                }
+
+            }
+
+        }
+
+        //剩下len+1个的时候，找出最大值，并删除即可
+        int max = -1;
+        for (int i : list) {
+            max = Math.max(max, i);
+        }
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) == max) {
+                continue;
+            }
+            System.out.print(list.get(i));
+        }
+
+
+    }
+
+    private static void demo() {
         Scanner sc = new Scanner(System.in);
 
         String s = sc.nextLine();
